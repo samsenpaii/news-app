71a2a62b9abd85ad3a6b35b4445c9511dac13fb6
Samarth Patil <samarthpatil2021.comp@mmcoe.edu.in>
2023-12-24 13:38:16 +0530

    first commit

---
+++ src/components/News.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import NewsItems from './NewsItems'
+
+export default class news extends Component {
+  render() {
+    return (
+      <div>
+        this is the news Component.
+        <NewsItems/>
+      </div>
+    )
+  }
+}
---
+++ src/components/NewsItems.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class NewsItems extends Component {
+  render() {
+    return (
+      <div>
+        this is the news item.
+      </div>
+    )
+  }
+}
+
+export default NewsItems
