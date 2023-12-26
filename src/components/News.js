15819f89a82d578a226a260878442ed0212c331c
Samarth Patil <samarthpatil2021.comp@mmcoe.edu.in>
2023-12-26 19:27:12 +0530

    same code

--- src/components/News.js
+++ src/components/News.js
@@ -2,43 +2,51 @@ import React, { Component } from 'react'
 import NewsItems from './NewsItems'
 
 export  class News extends Component {
-  articles =[
-    {
-      "source": { "id": "espn-cric-info", "name": "ESPN Cric Info" },
-      "author": null,
-      "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-      "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-      "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-      "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-      "publishedAt": "2020-04-27T11:41:47Z",
-      "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-    },
-    {
-      "source": { "id": "espn-cric-info", "name": "ESPN Cric Info" },
-      "author": null,
-      "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-      "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-      "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-      "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-      "publishedAt": "2020-03-30T15:26:05Z",
-      "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-    }
-  ]
+
 
   constructor(){
     super();
     this.state={
-      articles : this.articles,
-      loading : false
+      articles :[ ],
+      loading : false,
+      page : 1,
     }
   }
 
   async componentDidMount(){
-    let url ="https://newsapi.org/v2/top-headlines?country=in&apiKey=ccec2138afb34d03874b4aa9e947a4ea";
+    let url ="https://newsapi.org/v2/top-headlines?country=in&apiKey=ccec2138afb34d03874b4aa9e947a4ea&page=1&pageSize=20";
     let data = await fetch(url);
     let parsedData = await data.json()
-    this.setState({articles : parsedData.articles})
+    this.setState({articles : parsedData.articles , totalResults : parsedData.totalResults})
   }
+
+  handelPreClick = async()=>{
+    console.log("Previous clicked")
+    let url =`https://newsapi.org/v2/top-headlines?country=in&apiKey=ccec2138afb34d03874b4aa9e947a4ea&page=${this.state.page - 1}&pageSize=20`;
+    let data = await fetch(url);
+    let parsedData = await data.json()
+    this.setState({page : this.page -1,
+      articles : parsedData.articles})
+  }
+
+  handelNextClick = async()=>{
+
+    console.log("Next clickced")
+    if ( this.page +1 > Math.ceil(this.state.totalResults/20)){
+
+    }
+    else{
+      let url =`https://newsapi.org/v2/top-headlines?country=in&apiKey=ccec2138afb34d03874b4aa9e947a4ea&page=${this.state.page + 1}&pageSize=20`;
+      let data = await fetch(url);
+      let parsedData = await data.json()
+      this.setState({
+        page : this.page +1,
+        articles : parsedData.articles})
+    }
+    
+  }
+
+  
 
   render() {
     return (
@@ -53,6 +61,11 @@ export  class News extends Component {
             })}
               
           </div>
+          <div className="container d-flex justify-content-evenly">
+         <button type="button" className="btn btn-dark" disabled={this.state.page<=1} onClick={this.handelPreClick}>&larr; Pervious </button>
+         <button type="button" className="btn btn-dark" onClick={this.handelNextClick}>Next &rarr;</button>
+
+      </div>
       </div>
       </>
     )
--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -6,8 +6,8 @@ export class NewsItems extends Component {
     return (
       <>
       <div>
-        <div className="card" style={{width: '18rem'}}>
-            <img src={imageUrl} className="card-img-top" alt="..."/>
+        <div className="card mx-2 my-2" style={{width: '18rem'}}>
+            <img src={!imageUrl?"https://th.bing.com/th/id/R.9d7b29446a6adc7316a4b775dd8754ec?rik=JlaOShB8Otei%2bA&riu=http%3a%2f%2fwww.lowerycommunications.com%2fwp%2fwp-content%2fuploads%2f2013%2f12%2fslider-for-news-m.jpg&ehk=pBuBXaobIcXuoEzAUW8R4653wTMiXjzA58x80zL0UFM%3d&risl=&pid=ImgRaw&r=0":imageUrl} className="card-img-top" alt="..."/>
             <div className="card-body">
           <h5 className="card-title">{title}...</h5>
           <p className="card-text">
